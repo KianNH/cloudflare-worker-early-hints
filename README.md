60469b8bd58088a43e89bb26afde6ee8575c17b7
KianNH <kian@kian.org.uk>
2022-07-05 18:26:55 +0100

    chore: initial release

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-# Early Hints Worker - coming soon
+# Early Hints Worker
 
 A Cloudflare Worker that uses [HTMLRewriter](https://developers.cloudflare.com/workers/runtime-apis/html-rewriter/) in
 order to select assets from the page that we want to include in [Link](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Link)
 headers, so that Cloudflare can cache these and emit [103 Early Hints](https://developers.cloudflare.com/cache/about/early-hints/) responses.
 
+## Blog Post
+
+Curious about what each part of the code does and why it's used?
+
+Check out my [blog post!](https://kian.org.uk/implementing-103-early-hints-with-cloudflare-workers-htmlrewriter/)
+
 ## Requirements
 
-- [A Cloudflare Account](https://dash.cloudflare.com/sign-up/workers)
-- [Wrangler2](https://github.com/cloudflare/wrangler2)
+- [A Cloudflare Account](https://dash.cloudflare.com/sign-up/workers) and a website using Cloudflare.
+- [wrangler](https://github.com/cloudflare/wrangler2)
+
+## Instructions
+
+1. Clone the repository.
+2. Make sure that you `wrangler` logged in to your Cloudflare account.
+3. Open `wrangler.toml` and add a `route` key as necessary to make the Worker run on your website.
+```toml
+route = { pattern = "example.com/*", zone_name = "example.com" } 
+```
+4. If necessary, make any changes to the HTML element selectors. You don't want to preload lazy-loaded images, as an example.
+5. Run `wrangler publish` and the Worker will be live on your website!
---
+++ src/index.ts
@@ -0,0 +1,74 @@
+interface PreloadResourceHint {
+  url: string;
+  fileType: "style" | "image" | "script";
+}
+
+export default {
+  async fetch(request: Request, env: {}, ctx: ExecutionContext) {
+    ctx.passThroughOnException();
+
+    const response = await fetch(request);
+
+    if (response.status !== 200) {
+      console.log(`${request.url} returned (${response.status}), skipping`);
+      return response;
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (contentType === null || !contentType.startsWith("text/html")) {
+      console.log(`${request.url} is (${contentType}), skipping`);
+      return response;
+    }
+
+    const preloads: PreloadResourceHint[] = [];
+
+    class ElementHandler {
+      element(element: Element) {
+        switch (element.tagName) {
+          case "img": {
+            const url = element.getAttribute("src");
+            if (url && !url.startsWith("data:")) {
+              preloads.push({ url, fileType: "image" });
+            }
+            break;
+          }
+          case "script": {
+            const url = element.getAttribute("src");
+            if (url && !url.startsWith("data:")) {
+              preloads.push({ url, fileType: "script" });
+            }
+            break;
+          }
+          case "link": {
+            const url = element.getAttribute("href");
+            if (url && !url.startsWith("data:")) {
+              preloads.push({ url, fileType: "style" });
+            }
+            break;
+          }
+        }
+      }
+    }
+
+    const transformed = new HTMLRewriter()
+      .on("img", new ElementHandler())
+      .on("script", new ElementHandler())
+      .on("link[rel=stylesheet]", new ElementHandler())
+      .transform(response);
+
+    const body = await transformed.text();
+    const headers = new Headers(response.headers);
+
+    preloads.forEach((element) => {
+      headers.append(
+        "link",
+        `<${element.url}>; rel=preload; as=${element.fileType}`
+      );
+    });
+
+    return new Response(body, {
+      ...response,
+      headers: headers,
+    });
+  },
+};
